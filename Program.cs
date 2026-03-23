8f26f16855a06dcc5dc83d36bbeb9f0b6347f1dc
Meu Nome <joelma.carmo.duarte@gmail.com>
2026-03-22 23:20:25 -0300

    Exercício Música concluído com sucesso

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("\nCântico 134 - Imagine a Si Mesmo no Paraiso - Apocalipse 21:1-5\n");
+Console.ResetColor();
+
+Console.WriteLine("1. Já posso ver — você e eu");
+Console.WriteLine("   No Paraíso que Deus prometeu;");
+Console.WriteLine("   Quanta emoção, ao caminhar");
+Console.WriteLine("   Num mundo livre, feliz, em paz.");
+Console.WriteLine("   O mal não mais existirá;");
+Console.WriteLine("   O Reino justo não vai falhar");
+Console.WriteLine("   — Um novo começo, sem comparação;");
+Console.WriteLine("   A Deus cantaremos de todo o coração:\n");
+
+Console.ForegroundColor = ConsoleColor.Magenta;
+Console.WriteLine("(REFRÃO)\n");
+
+Console.WriteLine("“Jeová, nosso Deus, que grande prazer");
+Console.WriteLine(" Presenciar teu imenso poder!");
+Console.WriteLine(" Profundo amor sentimos por ti, ó Senhor;");
+Console.WriteLine(" És digno de glória, de honra e louvor.”\n");
+Console.ResetColor();
+
+Console.WriteLine("2. Eu posso até imaginar");
+Console.WriteLine("   O novo mundo em que vamos morar:");
+Console.WriteLine("   Nenhuma dor; temor não há.");
+Console.WriteLine("   Conosco a tenda de Deus está.");
+Console.WriteLine("   O que Jeová profetizou");
+Console.WriteLine("   Realidade já se tornou.");
+Console.WriteLine("   Agora os mortos irão despertar;");
+Console.WriteLine("   Com eles nós vamos ao nosso Deus cantar:\n");
+
+Console.ForegroundColor=ConsoleColor.Magenta;
+Console.WriteLine("(REFRÃO)\n");
+
+Console.WriteLine("“Jeová, nosso Deus, que grande prazer");
+Console.WriteLine(" Presenciar teu imenso poder!");
+Console.WriteLine(" Profundo amor sentimos por ti, ó Senhor;");
+Console.WriteLine(" És digno de glória, de honra e louvor.”\n");
+Console.ResetColor();
+
+Console.WriteLine("(Veja também Sal. 37:10, 11; Isa. 65:17; João 5:28; 2 Ped. 3:13.)\n");
+Console.WriteLine("Fonte de pesquisa: https://www.jw.org/pt/biblioteca/musicas-canticos/Cantemos-a-Jeová/Imagine-a-si-mesmo-no-Paraíso/\n");
